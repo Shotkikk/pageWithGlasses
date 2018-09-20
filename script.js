681349ed438c440980c02302f170d8b129de49da
Gawel <gawel.konefeld@homecenter.pl>
2018-09-20 17:42:57 +0200

    adding actions to slider, custom checkbox, states on list, fix @media

--- script.js
+++ script.js
@@ -1,33 +1,62 @@
-var btnWeiter = document.querySelector('.btnWeiter');
-var btnZuruck = document.querySelector('.btnZuruckContainer');
+function pageGallery() {
+    var btnWeiter = document.querySelector('.btnWeiter');
+    var changeGallery = true;
 
-btnWeiter.addEventListener('click', function(e){
+    btnWeiter.addEventListener('click', function(e){
+        changeGallery = !changeGallery;
+        console.log(e);
+        if(changeGallery === true) {
+            e.target.parentElement.style = 'justify-content: flex-end;';
+            e.target.parentElement.parentElement.children[0].style = 'display: flex';
+            e.target.parentElement.parentElement.children[1].style = 'display: none';
+            e.target.style = 'background-color: #6600a1';
+            e.target.children[0].style = 'clip-path: polygon(44% 0, 51% 0, 100% 50%, 51% 100%, 44% 100%, 92% 50%)';
+            e.target.parentElement.children[0].style = 'display: block';
+            e.target.parentElement.children[2].style = 'display: none';
+        }
+        else {
+            console.log(e.target.parentElement.parentElement.children, 'weiter');
+            e.target.parentElement.style = 'justify-content: flex-start;';
+            e.target.parentElement.parentElement.children[0].style = 'display: none';
+            e.target.parentElement.parentElement.children[1].style = 'display: block';
+            e.target.style = 'background-color: #EDEDED';
+            e.target.children[0].style = 'clip-path: polygon(100% 0, 48% 50%, 100% 100%, 92% 100%, 41% 50%, 92% 0)';
+            console.log(e.target.parentElement.children[2], );
+            e.target.parentElement.children[0].style = 'display: none';
+            e.target.parentElement.children[2].style = 'display: block';
 
-    var glassesGallery = document.querySelector('.glasses');
-    var slider = e.target.parentElement.previousElementSibling;
+        }
+    });
+}
 
-
-    console.log(glassesGallery)
-
-    e.target.parentElement.style = 'display: none';
-    slider.style = 'display: none';
-    glassesGallery.style = 'display: flex; justify-content: space-around';
-    btnZuruck.style = 'display: flex';
-
-})
-
-btnZuruck.addEventListener('click', function () {
-	slider.style = 'display: flex';
-})
-
-
+function changePage() {
+    var changeBtn = document.querySelector('.changePageLayout');
+    var toggleStyle = false;
+    changeBtn.addEventListener('click', function() {
+        toggleStyle = !toggleStyle
+        console.log(toggleStyle)
+        if(toggleStyle == true) {
+            var head  = document.getElementsByTagName('head')[0];
+            var link  = document.createElement('link');
+            link.rel  = 'stylesheet';
+            link.type = 'text/css';
+            link.href = './newStyle.css';
+            link.media = 'all';
+            head.appendChild(link);
+        }
+        else{
+            window.location.reload();
+        }
+    })
+}
 
 function addProducts(howManyProducts, howManyLi){
     var productsContainer = document.querySelector('.products');
     var productsDetail = [
         'Braillentyp: Damen, Herren', 'Rahmentyp: Randlose Brille', 'Eckiger Rahmen', 'Ovaler Rahmen', "Runder Rahmen", 'Still: casual, extravagant', 'Einstarkenbrille','Material: Metal'
     ];
-    var prices = ['ab 39,90€', 'ab 149,90€']
+    var prices = ['ab 39,90€', 'ab 149,90€'];
+    var liStatus = [1,1,1,1,1,1,0,0];
 
     for(var i = 0; i < howManyProducts; i++){
 
@@ -46,9 +75,6 @@ function addProducts(howManyProducts, howManyLi){
         var btnDescription = document.createElement('span');
         var btnShoppingCartIcon = document.createElement('i');
         var btnArrowRightIcon = document.createElement('i');
-
-
-
 
 
         newUl.classList = 'specsList';
@@ -77,13 +103,14 @@ function addProducts(howManyProducts, howManyLi){
             var check = document.createElement('i');
 
             checkContainer.classList = 'fa-li';
-            check.classList = 'fas fa-check check';
+            // liStatus[j] == 1 ? console.log(liStatus[j]) :  console.log(liStatus[j]);
+            liStatus[j] == 1 ?  check.classList = 'fas fa-check check' :  check.classList = 'fas fa-circle dot';
+            // check.classList = 'fas fa-check check';
 
             checkContainer.appendChild(check);
 
 			newLi.innerHTML = productsDetail[j];
 			newLi.appendChild(check);
-
 
             newUl.appendChild(newLi);
         }
@@ -111,11 +138,10 @@ function addProducts(howManyProducts, howManyLi){
 
 }
 
-document.addEventListener("DOMContentLoaded", function(event) {
-
+document.addEventListener("DOMContentLoaded", function() {
     addProducts(2,8);
-
-
+    pageGallery();
+    changePage();
 });
 
 
